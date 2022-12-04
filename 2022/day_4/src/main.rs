646362fbd02c6220354fd9b8fe575d5a68a19f3a
Bogdan <bogdanx904@gmail.com>
2022-12-04 19:04:01 +0200

    Day 4 template (testing)

---
+++ 2022/day_4/src/main.rs
@@ -0,0 +1,9 @@
+use std::fs;
+
+fn main() {
+    // TODO: Change to day_4/input.txt
+    let input = fs::read_to_string("input.txt").unwrap();
+    let input = input.trim();
+
+    println!("Hello, world!");
+}
