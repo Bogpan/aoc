dbe4dc0abfe0392475cb2252ed8350bbf9322cbd
Bogdan <bogdanx904@gmail.com>
2022-12-08 01:22:05 +0200

    Clean up day 6

--- 2022/day_6/src/main.rs
+++ 2022/day_6/src/main.rs
@@ -4,34 +4,26 @@ fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
     let input = input.trim();
 
-    let mut index1 = 0;
-    let mut index2 = 0;
-
-    for (i, _) in input.chars().enumerate() {
-        let recent: Vec<String> = input[i..i + 4].chars().map(|c| c.to_string()).collect();
-
-        if (1..recent.len()).any(|i| recent[i..].contains(&recent[i - 1])) {
-            continue;
-        } else {
-            let char = recent.join("");
-            index1 = input.find(&char).unwrap() + 4;
-
-            break;
-        }
-    }
-
-    for (i, _) in input.chars().enumerate() {
-        let recent: Vec<String> = input[i..i + 4].chars().map(|c| c.to_string()).collect();
-
-        if (1..recent.len()).any(|i| recent[i..].contains(&recent[i - 1])) {
-            continue;
-        } else {
-            let char = recent.join("");
-            index2 = input.find(&char).unwrap() + 14;
-
-            break;
-        }
-    }
+    let index1 = unique_index(input, 4);
+    let index2 = unique_index(input, 14);
 
     println!("Part 1: {}\nPart 2: {}", index1, index2);
+}
+
+fn unique_index(string: &str, offset: usize) -> usize {
+    for (i, _) in string.chars().enumerate() {
+        let recent: Vec<String> = string[i..i + offset]
+            .chars()
+            .map(|c| c.to_string())
+            .collect();
+
+        if (1..recent.len()).any(|i| recent[i..].contains(&recent[i - 1])) {
+            continue;
+        } else {
+            let char = recent.join("");
+            return string.find(&char).unwrap() + offset;
+        }
+    }
+
+    0
 }
