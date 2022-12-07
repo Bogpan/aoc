1976a313dea390fdfa324630ef0e8f1932f355a3
Bogdan <bogdanx904@gmail.com>
2022-12-08 01:16:15 +0200

    Tidy up day 5 folder and do day 6

--- 2022/day_6/src/main.rs
+++ 2022/day_6/src/main.rs
@@ -1,3 +1,37 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input.txt").unwrap();
+    let input = input.trim();
+
+    let mut index1 = 0;
+    let mut index2 = 0;
+
+    for (i, _) in input.chars().enumerate() {
+        let recent: Vec<String> = input[i..i + 4].chars().map(|c| c.to_string()).collect();
+
+        if (1..recent.len()).any(|i| recent[i..].contains(&recent[i - 1])) {
+            continue;
+        } else {
+            let char = recent.join("");
+            index1 = input.find(&char).unwrap() + 4;
+
+            break;
+        }
+    }
+
+    for (i, _) in input.chars().enumerate() {
+        let recent: Vec<String> = input[i..i + 4].chars().map(|c| c.to_string()).collect();
+
+        if (1..recent.len()).any(|i| recent[i..].contains(&recent[i - 1])) {
+            continue;
+        } else {
+            let char = recent.join("");
+            index2 = input.find(&char).unwrap() + 14;
+
+            break;
+        }
+    }
+
+    println!("Part 1: {}\nPart 2: {}", index1, index2);
 }
